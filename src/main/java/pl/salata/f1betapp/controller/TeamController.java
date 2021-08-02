27d63db33507f04091b14f2e57871825ef3d80a4
Salata <pawemie@gmail.com>
2021-08-02 23:44:07 +0200

    added endpoint to fetch by id added Cors annotation

--- src/main/java/pl/salata/f1betapp/controller/TeamController.java
+++ src/main/java/pl/salata/f1betapp/controller/TeamController.java
@@ -1,16 +1,14 @@
 package pl.salata.f1betapp.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.salata.f1betapp.model.Team;
 import pl.salata.f1betapp.service.TeamService;
 
 @AllArgsConstructor
 @RequestMapping("teams")
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private final TeamService teamService;
@@ -18,5 +16,10 @@ public class TeamController {
     @GetMapping
     public Team getByName(@RequestParam String name) {
         return teamService.getByName(name);
+    }
+
+    @GetMapping("{id}")
+    public Team getTeam(@PathVariable Long id) {
+        return teamService.findById(id);
     }
 }
