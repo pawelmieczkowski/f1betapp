9fa5f97e4a039bcd1af5f482f3c9db6718950f52
Salata <pawemie@gmail.com>
2021-08-04 21:28:22 +0200

    header on mobile is immune to horizontal scrolling

--- src/frontend/src/components/common/header/Header.js
+++ src/frontend/src/components/common/header/Header.js
@@ -6,11 +6,13 @@ import { Navbar } from '../../common'
 function Header() {
   return (
     <section className="header">
-      <section className="header-logo">
-        <a href='/' >LOGO</a>
-      </section>
-      <section className="header-navbar">
-        <Navbar />
+      <section className="page">
+        <section className="header-logo">
+          <a href='/' >LOGO</a>
+        </section>
+        <section className="header-navbar">
+          <Navbar />
+        </section>
       </section>
     </section>
   );
