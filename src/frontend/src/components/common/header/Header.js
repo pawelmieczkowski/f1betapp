521b56b80fe0444beba92b20cf3463d0f26c5836
Salata <pawemie@gmail.com>
2021-08-07 18:07:32 +0200

    Added login button

--- src/frontend/src/components/common/header/Header.js
+++ src/frontend/src/components/common/header/Header.js
@@ -6,14 +6,15 @@ import { Navbar } from '../../common'
 function Header() {
   return (
     <section className="header">
-      <section className="page">
         <section className="header-logo">
           <a href='/' >LOGO</a>
         </section>
         <section className="header-navbar">
           <Navbar />
         </section>
-      </section>
+        <section className="header-login">
+          <a href='/login'>Login</a>
+        </section>
     </section>
   );
 }
