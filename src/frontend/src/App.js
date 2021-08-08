ce2e13043c445c8de60dfb549126459aa299d944
Salata <pawemie@gmail.com>
2021-08-08 23:29:50 +0200

    login page init

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,35 +1,42 @@
 import './App.scss';
-import { Header } from './components/common'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { GrandPrixArchivePage } from './pages/GrandPrixArchivePage';
-import { RaceResultPage } from './pages/RaceResultPage';
-import { DriverPage } from './pages/DriverPage';
-import { TeamPage } from './pages/TeamPage';
+import React from 'react';
+import {Header} from './components/common'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {GrandPrixArchivePage} from './pages/GrandPrixArchivePage';
+import {RaceResultPage} from './pages/RaceResultPage';
+import {DriverPage} from './pages/DriverPage';
+import {TeamPage} from './pages/TeamPage';
+import {Register} from './components/Register';
+import {Login} from './components/Login';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div className="page-content">
-        <Router>
-          <Switch>
-            <Route path="/race-result/:grandPrixId">
-              <RaceResultPage />
-            </Route>
-            <Route path="/drivers/:driverId">
-              <DriverPage />
-            </Route>
-            <Route path="/teams/:teamName">
-              <TeamPage />
-            </Route>
-            <Route path="/">
-              <GrandPrixArchivePage />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <div className="page-content">
+                <Router>
+                    <Switch>
+                        <Route path="/race-result/:grandPrixId">
+                            <RaceResultPage/>
+                        </Route>
+                        <Route path="/drivers/:driverId" component={DriverPage}/>
+                        <Route path="/teams/:teamName">
+                            <TeamPage/>
+                        </Route>
+                        <Route exact path="/register">
+                            <Register/>
+                        </Route>
+                        <Route exact path="/login">
+                            <Login/>
+                        </Route>
+                        <Route path="/">
+                            <GrandPrixArchivePage/>
+                        </Route>
+                    </Switch>
+                </Router>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/frontend/src/services/auth.service.js
+++ src/frontend/src/services/auth.service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_REGISTRATION_URL = "http://localhost:8080/registration"
+const API_LOGIN_URL = "http://localhost:8080/login"
 
 const register = (username, email, password) => {
     return axios.post(API_REGISTRATION_URL, {
@@ -10,9 +11,29 @@ const register = (username, email, password) => {
     });
   };
 
-
+  const login = async (username, password) => {
+    const response = await axios
+          .post(API_LOGIN_URL, {
+              username,
+              password,
+          });
+      if (response.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+      }
+      return response.data;
+  };
+  
+  const logout = () => {
+    localStorage.removeItem("user");
+  };
+  
+  const getCurrentUser = () => {
+    return JSON.parse(localStorage.getItem("user"));
+  };
   
   export default {
     register,
-
+    login,
+    logout,
+    getCurrentUser
   };
