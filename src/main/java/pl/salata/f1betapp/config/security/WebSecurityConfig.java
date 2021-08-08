946722893587a99970621a57ee9c75f3fdbe286b
Salata <pawemie@gmail.com>
2021-08-08 17:06:12 +0200

    implemented JWT

--- src/main/java/pl/salata/f1betapp/config/security/WebSecurityConfig.java
+++ src/main/java/pl/salata/f1betapp/config/security/WebSecurityConfig.java
@@ -1,24 +1,47 @@
 package pl.salata.f1betapp.config.security;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.cors.CorsUtils;
-import pl.salata.f1betapp.appuser.AppUserService;
+import pl.salata.f1betapp.login.JsonObjectAuthenticationFilter;
+import pl.salata.f1betapp.login.JwtAuthorizationFilter;
+import pl.salata.f1betapp.login.RestAuthenticationFailureHandler;
+import pl.salata.f1betapp.login.RestAuthenticationSuccessHandler;
+import pl.salata.f1betapp.login.appuser.AppUserService;
 
 @Configuration
-@AllArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final AppUserService appUserService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ObjectMapper objectMapper;
+    private final RestAuthenticationSuccessHandler successHandler;
+    private final RestAuthenticationFailureHandler failureHandler;
+    private final String secret;
+
+    public WebSecurityConfig(AppUserService appUserService, BCryptPasswordEncoder bCryptPasswordEncoder,
+                             ObjectMapper objectMapper, RestAuthenticationSuccessHandler successHandler,
+                             RestAuthenticationFailureHandler failureHandler,
+                             @Value("${jwt.secret}") String secret) {
+        this.appUserService = appUserService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.objectMapper = objectMapper;
+        this.successHandler = successHandler;
+        this.failureHandler = failureHandler;
+        this.secret = secret;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -38,11 +61,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin();
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(authenticationFilter())
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(), appUserService, secret))
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    protected void configure(AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(daoAuthenticationProvider());
     }
 
@@ -52,5 +81,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         provider.setPasswordEncoder(bCryptPasswordEncoder);
         provider.setUserDetailsService(appUserService);
         return provider;
+    }
+
+    public JsonObjectAuthenticationFilter authenticationFilter() throws Exception {
+        JsonObjectAuthenticationFilter authenticationFilter = new JsonObjectAuthenticationFilter(objectMapper);
+        authenticationFilter.setAuthenticationSuccessHandler(successHandler);
+        authenticationFilter.setAuthenticationFailureHandler(failureHandler);
+        authenticationFilter.setAuthenticationManager(super.authenticationManager());
+        return authenticationFilter;
     }
 }
---
+++ src/main/java/pl/salata/f1betapp/login/LoginCredentials.java
@@ -0,0 +1,9 @@
+package pl.salata.f1betapp.login;
+
+import lombok.Getter;
+
+@Getter
+public class LoginCredentials {
+    private String username;
+    private String password;
+}
