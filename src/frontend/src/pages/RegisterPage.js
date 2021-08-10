86cd614875e07a2203f267a0bc3d3a742a80ce74
Salata <pawemie@gmail.com>
2021-08-10 20:41:27 +0200

    styled register page

--- src/frontend/src/pages/RegisterPage.js
+++ src/frontend/src/pages/RegisterPage.js
@@ -1,3 +1,4 @@
+import "./RegisterPage.scss"
 import { React, useState } from "react";
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -31,7 +32,6 @@ export const Register = () => {
     const {
         register,
         handleSubmit,
-        reset,
         formState: { errors }
     } = useForm({
         resolver: yupResolver(validationSchema)
@@ -62,71 +62,67 @@ export const Register = () => {
     };
 
     return (
-        <div className="register-form">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="form-group">
-                    <label>Username</label>
-                    <input
-                        name="username"
-                        type="text"
-                        {...register('username')}
-                        className={`form-control ${errors.username ? 'is-invalid' : ''}`}
-                    />
-                    <div className="invalid-feedback">{errors.username?.message}</div>
-                </div>
-
-                <div className="form-group">
-                    <label>Email</label>
-                    <input
-                        name="email"
-                        type="text"
-                        {...register('email')}
-                        className={`form-control ${errors.email ? 'is-invalid' : ''}`}
-                    />
-                    <div className="invalid-feedback">{errors.email?.message}</div>
-                </div>
-
-                <div className="form-group">
-                    <label>Password</label>
-                    <input
-                        name="password"
-                        type="password"
-                        {...register('password')}
-                        className={`form-control ${errors.password ? 'is-invalid' : ''}`}
-                    />
-                    <div className="invalid-feedback">{errors.password?.message}</div>
-                </div>
-
-                <div className="form-group">
-                    <label>Confirm Password</label>
-                    <input
-                        name="confirmPassword"
-                        type="password"
-                        {...register('confirmPassword')}
-                        className={`form-control ${errors.confirmPassword ? 'is-invalid' : ''
-                            }`}
-                    />
-                    <div className="invalid-feedback">
-                        {errors.confirmPassword?.message}
+        <div className="RegisterPage">
+            <div className="form-group">
+                <h1 className="headline">
+                    Create Account
+                </h1>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-field">
+                        <input
+                            placeholder="Username"
+                            name="username"
+                            type="text"
+                            {...register('username')}
+                            className={`form-control ${errors.username ? 'is-invalid' : ''}`}
+                        />
+                        <div className="invalid-feedback">{errors.username?.message}</div>
                     </div>
-                </div>
 
-                <div className="form-group">
-                    <button type="submit" className="btn btn-primary">
-                        Register
-                    </button>
-                    <button
-                        type="button"
-                        onClick={() => reset()}
-                        className="btn btn-warning float-right"
-                    >
-                        Reset
-                    </button>
-                </div>
-            </form>
+                    <div className="form-field">
+                        <input
+                            placeholder="Email"
+                            name="email"
+                            type="text"
+                            {...register('email')}
+                            className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                        />
+                        <div className="invalid-feedback">{errors.email?.message}</div>
+                    </div>
 
-            {message && (
-                <div className="form-group">
+                    <div className="form-field">
+                        <input
+                            placeholder="Password"
+                            name="password"
+                            type="password"
+                            {...register('password')}
+                            className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+                        />
+                        <div className="invalid-feedback">{errors.password?.message}</div>
+                    </div>
+
+                    <div className="form-field">
+                        <input
+                            placeholder="Confirm password"
+                            name="confirmPassword"
+                            type="password"
+                            {...register('confirmPassword')}
+                            className={`form-control ${errors.confirmPassword ? 'is-invalid' : ''
+                                }`}
+                        />
+                        <div className="invalid-feedback">
+                            {errors.confirmPassword?.message}
+                        </div>
+                    </div>
+
+                    <div className="form-field">
+                        <button type="submit" className="register-button">
+                            Register
+                        </button>
+                    </div>
+                </form>
+
+                {message && (
                     <div
                         className={
                             successful ? "alert alert-success" : "alert alert-danger"
@@ -135,8 +131,8 @@ export const Register = () => {
                     >
                         {message}
                     </div>
-                </div>
-            )}
+                )}
+            </div>
         </div>
     );
 };
