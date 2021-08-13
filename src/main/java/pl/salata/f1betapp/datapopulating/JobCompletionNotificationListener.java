8e6ea790f3a8644e7237068b2639ac3d57ce9fee
Salata <pawemie@gmail.com>
2021-08-13 19:31:09 +0200

    refactor

--- src/main/java/pl/salata/f1betapp/datapopulating/JobCompletionNotificationListener.java
+++ src/main/java/pl/salata/f1betapp/datapopulating/JobCompletionNotificationListener.java
@@ -5,33 +5,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import pl.salata.f1betapp.model.Circuit;
 
 @Component
 public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
 
     private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
-//    private final JdbcTemplate jdbcTemplate;
-//
-//    @Autowired
-//    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
-//        this.jdbcTemplate = jdbcTemplate;
-//    }
-
     @Override
     public void afterJob(JobExecution jobExecution) {
         if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
             log.info("!!! JOB FINISHED! Time to verify the results");
 
-//            jdbcTemplate.query("SELECT name, country FROM circuit",
-//                    (rs, row) -> new Circuit(
-//                            rs.getString(1),
-//                            rs.getString(2))
-//            ).forEach(circuit -> log.info("Found <" + circuit.getName() + " " + circuit.getCountry() + "> in the database."));
         }
     }
 }
