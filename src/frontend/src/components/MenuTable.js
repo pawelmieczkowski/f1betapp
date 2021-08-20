d5400d0df31f96a4cd4561c99f64b3101ac77148
Salata <pawemie@gmail.com>
2021-08-20 11:34:40 +0200

    extracted table into separate component

---
+++ src/frontend/src/components/MenuTable.js
@@ -0,0 +1,164 @@
+import React, { useState, useEffect, useMemo } from 'react'
+import {
+    useTable,
+    usePagination,
+    useFilters
+} from 'react-table'
+import { matchSorter } from 'match-sorter'
+import './MenuTable.scss'
+
+// Define a default UI for filtering
+function DefaultColumnFilter({
+    column: { filterValue, preFilteredRows, setFilter },
+}) {
+    const count = preFilteredRows.length
+
+    return (
+        <input
+            value={filterValue || ''}
+            onChange={e => {
+                setFilter(e.target.value || undefined)
+            }}
+            placeholder={`Search ${count} records...`}
+        />
+    )
+}
+
+function fuzzyTextFilterFn(rows, id, filterValue) {
+    return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
+}
+
+// Let the table remove the filter if the string is empty
+fuzzyTextFilterFn.autoRemove = val => !val
+
+export const MenuTable = (props) => {
+    const columns = useMemo(() => props.columns, []);
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        setResults(props.results)
+    }, [props])
+
+    const filterTypes = React.useMemo(
+        () => ({
+            fuzzyText: fuzzyTextFilterFn,
+        }),
+        []
+    )
+    const defaultColumn = React.useMemo(
+        () => ({
+            //default Filter UI
+            Filter: DefaultColumnFilter,
+        }),
+        []
+    )
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        prepareRow,
+        page,
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        state: { pageIndex, pageSize },
+    } = useTable(
+        {
+            columns: columns,
+            data: results,
+            defaultColumn,
+            filterTypes,
+            initialState: { pageIndex: 0, pageSize: 20 }
+        },
+        useFilters,
+        usePagination
+    )
+
+    return (
+        <section className="MenuTable">
+            <div className='scrollable'>
+                <table className="table" {...getTableProps()}>
+                    <thead>{
+                        headerGroups.map(headerGroup => (
+                            <tr {...headerGroup.getHeaderGroupProps()}>
+                                {headerGroup.headers.map(column => (
+                                    <th {...column.getHeaderProps({
+                                        style: {
+                                            width: column.width
+                                        }
+                                    })} >{column.render('Header')}
+                                        <div>{column.canFilter ? column.render('Filter') : null}</div>
+                                    </th>
+                                ))}
+                            </tr>
+                        ))
+                    }
+                        <tr>
+                        </tr>
+                    </thead>
+                    <tbody className="table-body" {...getTableBodyProps()}>
+                        {page.map((row) => {
+                            prepareRow(row)
+                            return (
+                                <tr {...row.getRowProps()} className="table-row">
+                                    {row.cells.map(cell => {
+                                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                    })}
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            <div className="pagination">
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    {'<<'}
+                </button>{' '}
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    {'<'}
+                </button>{' '}
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                    {'>'}
+                </button>{' '}
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                    {'>>'}
+                </button>{' '}
+                <span>
+                    Page{' '}
+                    <strong>
+                        {pageIndex + 1} of {pageOptions.length}
+                    </strong>{' '}
+                </span>
+                <span>
+                    | Go to page:{' '}
+                    <input
+                        type="number"
+                        defaultValue={pageIndex + 1}
+                        onChange={e => {
+                            const page = e.target.value ? Number(e.target.value) - 1 : 0
+                            gotoPage(page)
+                        }}
+                    />
+                </span>{' '}
+                <select
+                    value={pageSize}
+                    onChange={e => {
+                        setPageSize(Number(e.target.value))
+                    }}
+                >
+                    {[10, 20, 30, 40, 50].map(pageSize => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </section>
+    );
+}
