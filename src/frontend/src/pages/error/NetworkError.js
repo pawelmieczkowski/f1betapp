bd07b20e744c250e91251aa89549eab26dce00de
Salata <pawemie@gmail.com>
2021-08-26 00:45:33 +0200

    styling

--- src/frontend/src/pages/error/NetworkError.js
+++ src/frontend/src/pages/error/NetworkError.js
@@ -1,9 +1,13 @@
+import './NetworkError.scss'
 import React from "react";
 import { Link } from "react-router-dom";
 
 export const NetworkError = () => (
-  <div>
-    <h1>Something went wrong</h1>
-    <Link to="/">Back to homepage</Link>
+  <div className='NetworkError'>
+    <div className='message'>
+      <h1>Something went wrong</h1>
+      <h2>Server not responding</h2>
+      <Link to="/">Back to Homepage</Link>
+    </div>
   </div>
 );
--- src/frontend/src/pages/error/Page404.js
+++ src/frontend/src/pages/error/Page404.js
@@ -1,9 +1,18 @@
+import './Error.scss'
 import React from "react";
-import { Link } from "react-router-dom";
 
 export const Page404 = () => (
-  <div>
-    <h1>Error 404</h1>
-    <Link to="/">Back to homepage</Link>
+  <div className='Error'>
+    <div className='wrap'>
+      <div className='status'>
+        <p>
+          Error
+        </p>
+        <p className='code'>
+          404
+        </p>
+      </div>
+      <div className='message'>Page Not Found</div>
+    </div>
   </div>
 );
